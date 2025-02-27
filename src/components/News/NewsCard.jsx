b1e8a8e2af1e2e90b1b4be4089b8133c63d5efdb
sribabu <sribabumandraju@gmail.com>
2025-02-27 22:34:10 +0530

    new changes by sri

--- src/components/News/NewsCard.jsx
+++ src/components/News/NewsCard.jsx
@@ -1,8 +1,8 @@
-import ReporterInfo from "./newsCard_components/ReporterInfo"
-import NewsDetails from "./newsCard_components/NewsDetails"
-import MediaContent from "./newsCard_components/MediaContent"
-import LocationAndTime from "./newsCard_components/LocationAndTime"
-import InteractionButtons from "./newsCard_components/InteractionButtons"
+import ReporterInfo from "./newsCard_components/ReporterInfo";
+import NewsDetails from "./newsCard_components/NewsDetails";
+import MediaContent from "./newsCard_components/MediaContent";
+import LocationAndTime from "./newsCard_components/LocationAndTime";
+import InteractionButtons from "./newsCard_components/InteractionButtons";
 
 const NewsCard = ({ data }) => {
   return (
@@ -10,11 +10,19 @@ const NewsCard = ({ data }) => {
       <ReporterInfo posted_by={data.posted_by} />
       <NewsDetails news={data.news} />
       <MediaContent news={data.news} />
-      <LocationAndTime states={data.states} district={data.district} mandal={data.mandal} createdAt={data.createdAt} />
-      <InteractionButtons likes={data.likes} views={data.views} comments={data.comments} />
+      <LocationAndTime
+        states={data.states}
+        district={data.district}
+        mandal={data.mandal}
+        createdAt={data.createdAt}
+      />
+      <InteractionButtons
+        likes={data.likes}
+        views={data.views}
+        comments={data.comments}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default NewsCard
-
+export default NewsCard;
--- src/components/News/PostFeed.jsx
+++ src/components/News/PostFeed.jsx
@@ -1,9 +1,46 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useInView } from "react-intersection-observer";
-import { FaSpinner } from "react-icons/fa";
 import NewsCard from "./NewsCard";
 import { fetchPosts, clearFeed } from "../../store/feedSlice";
+
+const SkeletonLoader = () => (
+  <div className="bg-white dark:bg-gray-900 mt-3 rounded-lg shadow-lg p-4 w-[93%] md:w-full mx-auto border dark:border-gray-700 animate-pulse">
+    {/* Reporter Info Skeleton */}
+    <div className="flex items-start space-x-4">
+      <div className="w-10 h-10 bg-gray-200 dark:bg-gray-700 rounded-full" />
+      <div>
+        <div className="h-4 w-32 bg-gray-200 dark:bg-gray-700 rounded" />
+        <div className="h-3 w-20 bg-gray-200 dark:bg-gray-700 rounded mt-2" />
+      </div>
+    </div>
+
+    {/* News Details Skeleton */}
+    <div className="mt-3">
+      <div className="h-6 w-3/4 bg-gray-200 dark:bg-gray-700 rounded" />
+      <div className="h-4 w-full bg-gray-200 dark:bg-gray-700 rounded mt-2" />
+      <div className="h-4 w-2/3 bg-gray-200 dark:bg-gray-700 rounded mt-2" />
+    </div>
+
+    {/* Media Content Skeleton */}
+    <div className="mt-3 h-48 bg-gray-200 dark:bg-gray-700 rounded-lg" />
+
+    {/* Location and Time Skeleton */}
+    <div className="mt-3 flex items-center">
+      <div className="h-3 w-48 bg-gray-200 dark:bg-gray-700 rounded" />
+      <div className="h-3 w-32 bg-gray-200 dark:bg-gray-700 rounded ml-2" />
+    </div>
+
+    {/* Interaction Buttons Skeleton */}
+    <div className="mt-3 flex justify-between">
+      <div className="flex space-x-4">
+        <div className="h-4 w-16 bg-gray-200 dark:bg-gray-700 rounded" />
+        <div className="h-4 w-16 bg-gray-200 dark:bg-gray-700 rounded" />
+        <div className="h-4 w-16 bg-gray-200 dark:bg-gray-700 rounded" />
+      </div>
+    </div>
+  </div>
+);
 
 const PostFeed = () => {
   const dispatch = useDispatch();
@@ -35,9 +72,12 @@ const PostFeed = () => {
 
       {/* Infinite Scroll Trigger */}
       {hasMore && (
-        <div ref={ref} className="py-4 flex justify-center">
+        <div ref={ref} className="w-full">
           {loading && (
-            <FaSpinner className="text-gray-600 dark:text-gray-300 animate-spin text-2xl" />
+            <div className="flex flex-col gap-4 w-full">
+              <SkeletonLoader />
+              <SkeletonLoader />
+            </div>
           )}
         </div>
       )}
