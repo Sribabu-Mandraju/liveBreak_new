f9c835772610eb2384bfed7d073a461df9f08ede
sribabu <sribabumandraju@gmail.com>
2025-03-04 14:44:07 +0530

    new

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -28,9 +28,11 @@ const Dropdown = ({ label, items, isMobile, isOpen, toggleDropdown, icon }) => (
     <div
       className="flex items-center justify-between  font-semibold text-gray-700 dark:text-gray-300 hover:text-blue-700 dark:hover:text-blue-400 py-2 px-3 rounded-md transition-colors duration-200"
       onClick={toggleDropdown}
-    > <div className="flex flex-row items-center gap-1 ">
-      {icon && <span className="mr-2">{icon}</span>}
-      {label}
+    >
+      {" "}
+      <div className="flex flex-row items-center gap-1 ">
+        {icon && <span className="mr-2">{icon}</span>}
+        {label}
       </div>
       {items && (
         <FaAngleDown
@@ -67,7 +69,7 @@ const Navbar = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
   const [active, setActive] = useState("");
   const [isFocused, setIsFocused] = useState(false);
-  const [fields,setFields]=useState([]);
+  const [fields, setFields] = useState([]);
   const [loading, setLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const BASE_URL = import.meta.env.VITE_BASE_URL;
@@ -104,19 +106,15 @@ const Navbar = () => {
   ];
 
   const fetchfields = async (e) => {
-    setSearchQuery(e.target.value)
-    
+    setSearchQuery(e.target.value);
+
     setLoading(true);
     try {
-      const response = await axios.post(
-        `${BASE_URL}/common/tags`,
-        {
-          key:searchQuery,
-          type:"new",
-          version: "new",
-        },
-        
-      );
+      const response = await axios.post(`${BASE_URL}/common/tags`, {
+        key: searchQuery,
+        type: "new",
+        version: "new",
+      });
 
       console.log(response.data.data);
       setFields(response.data.data || []);
@@ -185,7 +183,6 @@ const Navbar = () => {
                 placeholder="Search"
                 className="search-bar"
                 onFocus={() => setIsFocused(true)}
-                
               />
               <IoMdSearch className="text-xl top-[20%]  text-gray-600 group-hover:text-blue-500 dark:text-gray-400 absolute  right-3 duration-200" />
             </div>
@@ -303,39 +300,47 @@ const Navbar = () => {
                 </Link>
               </div>
             )}
-
-            
           </div>
         </div>
         {isFocused && (
-              <div>
-                <Model isOpen={isFocused} onClose={() => setIsFocused(false)} title="Search">
-                  <div className="flex flex-col gap-4">
-                    <div className="relative my-4">
-                      <input type="text" placeholder="Search" value={searchQuery}
-          onChange={fetchfields} className="w-full bg-blue-50 dark:bg-gray-900 px-4 py-2 outline-none border dark:border-gray-700 border-gray-400 rounded-lg"/>
-                      <IoMdSearch className="text-xl top-[20%]  text-gray-600  dark:text-gray-400 absolute  right-3 duration-200" />
-                    </div>
-                    <hr className="dark:text-gray-600"/>
-                    <div className="flex flex-col">
-                      <div className="text-xl text-blue-500">Trending</div>
-                      
-                      <div className="flex flex-wrap gap-4 mt-4 max-h-[30vh] overflow-y-scroll">
-                        {fields.map((data)=>(
-                          <div className="bg-gray-200 dark:bg-gray-900 cursor-pointer px-2 py-1 rounded-xl" onClick={()=>{setSearchQuery(data.name)}}>
-                            {data.name}
-                          </div>
-                        ))
-                        }
+          <div>
+            <Model
+              isOpen={isFocused}
+              onClose={() => setIsFocused(false)}
+              title="Search"
+            >
+              <div className="flex flex-col gap-4">
+                <div className="relative my-4">
+                  <input
+                    type="text"
+                    placeholder="Search"
+                    value={searchQuery}
+                    onChange={fetchfields}
+                    className="w-full bg-blue-50 dark:bg-gray-900 px-4 py-2 outline-none border dark:border-gray-700 border-gray-400 rounded-lg"
+                  />
+                  <IoMdSearch className="text-xl top-[20%]  text-gray-600  dark:text-gray-400 absolute  right-3 duration-200" />
+                </div>
+                <hr className="dark:text-gray-600" />
+                <div className="flex flex-col">
+                  <div className="text-xl text-blue-500">Trending</div>
 
-
+                  <div className="flex flex-wrap gap-4 mt-4 max-h-[30vh] overflow-y-scroll">
+                    {fields.map((data) => (
+                      <div
+                        className="bg-gray-200 dark:bg-gray-900 cursor-pointer px-2 py-1 rounded-xl"
+                        onClick={() => {
+                          setSearchQuery(data.name);
+                        }}
+                      >
+                        {data.name}
                       </div>
-
-                    </div>
+                    ))}
                   </div>
-                </Model>
+                </div>
               </div>
-            )}
+            </Model>
+          </div>
+        )}
 
         {isMobileMenuOpen && (
           <>
@@ -356,16 +361,19 @@ const Navbar = () => {
                 <FaTimes size={24} />
               </button>
               <div className="flex justify-end mt-12">
-                <Mode/>
+                <Mode />
               </div>
               <div>
-              <div className="relative mt-6">
-                      <input type="text" placeholder="Search" onFocus={() => setIsFocused(true)}
-           className="w-full bg-blue-50 dark:bg-gray-900 px-4 py-2 outline-none border dark:border-gray-700 border-gray-400 rounded-lg"/>
-                      <IoMdSearch className="text-xl top-[20%]  text-gray-600  dark:text-gray-400 absolute  right-3 duration-200" />
-                    </div>
+                <div className="relative mt-6">
+                  <input
+                    type="text"
+                    placeholder="Search"
+                    onFocus={() => setIsFocused(true)}
+                    className="w-full bg-blue-50 dark:bg-gray-900 px-4 py-2 outline-none border dark:border-gray-700 border-gray-400 rounded-lg"
+                  />
+                  <IoMdSearch className="text-xl top-[20%]  text-gray-600  dark:text-gray-400 absolute  right-3 duration-200" />
+                </div>
               </div>
-
 
               <nav className="flex flex-col justify-start gap-2 mt-6">
                 {navItems.map((item, index) => (
